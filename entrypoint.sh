55a679924afa571c9a9de86a75b58a999fb7822b
katabame <ayame@kataba.me>
2023-05-17 21:56:59 +0900

    Fix: [server] server file download url 📄

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
-#cd /server
+# Make sure working directory is correct
+cd /server
 
 # Download latest build
-fileName=$(curl -s -X 'GET' -H 'accept: application/json' 'https://ci.loohpjames.com/job/Limbo/lastSuccessfulBuild/api/json' | jq '.artifacts[1].fileName')
-curl -s -o '/server/limbo.jar' "https://ci.loohpjames.com/job/Limbo/lastSuccessfulBuild/artifacts/${fileName}"
+fileName=$(curl -fsSL -X 'GET' -H 'accept: application/json' 'https://ci.loohpjames.com/job/Limbo/lastSuccessfulBuild/api/json' | jq -r '.artifacts[1].fileName')
+curl -fsSL -o 'limbo.jar' "https://ci.loohpjames.com/job/Limbo/lastSuccessfulBuild/artifact/target/${fileName}"
 
 # Run
 java -Xmx500M -Xms500M -jar limbo.jar --nogui
